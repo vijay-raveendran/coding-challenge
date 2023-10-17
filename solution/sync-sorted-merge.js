d1454626d757340e3eba82065a876e1d7bcd81ae
Vijay Raveendran <vijay.sriraam@outlook.com>
2023-10-16 22:54:30 -0700

    feat: finish sync portion

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,35 @@
 "use strict";
-
+import { SortedLinkedList, SortedLinkedListNode } from "./sorted-linked-list";
 // Print all entries, across all of the sources, in chronological order.
 
+/**
+ * Thoughts:
+ * 1. Prob wanna use a sorted linked list 
+ * Advantages are adding is O(n)
+ * Printing is O(n)/
+ * Could use secondary data structure to store pointers to specific elements to make insertion faster
+ */
+
+/**
+ * 
+ * @param {*} logSources 
+ * @param {*} printer 
+ * @returns 
+ */
+
+
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+
+  const ll = new SortedLinkedList(printer);
+
+  for (let i = 0; i < logSources.length; i++) {
+    ll.insert(logSources[i]);
+  }
+  ll.print();
+  printer.done();
 };
+
+// module.exports = {
+//   SortedLinkedList,
+//   SortedLinkedListNode
+// }
